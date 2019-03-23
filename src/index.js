3321c3d685e5c6e11aa834393cf8d4c45b3613cb
sarada <sarar3382@gmail.com>
2019-03-22 23:23:11 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, Switch} from 'react-router-dom';
+import App from './components/App';
+
+import './index.css';
+
+ReactDOM.render(<App/>    
+    , document.getElementById('root'));
